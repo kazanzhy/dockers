7cf7335bb04408bcb7c5a5b1a7020005ecddc104
kazanzhy <dkazanzhy@gmail.com>
2021-09-17 03:44:19 +0300

    Working Compose

--- superset/compose-init.sh
+++ superset/compose-init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sleep 10;
+sleep 5;
 superset db upgrade;
 superset fab create-admin --username admin --password admin --firstname Superset --lastname Admin --email admin@fab.org;
 superset init;
